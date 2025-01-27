d5e02a185235b8900ec240c2a159531f7ffb7145
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-27 15:24:27 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS Authors (
 -- Create 'customers' table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     phone VARCHAR(15),
     address TEXT
 );
