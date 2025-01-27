33c110e1e6a2b438afb57fa55ddc41a99fe61ed9
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-27 15:25:24 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     customer_id INT NOT NULL,
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     total_amount DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create 'order_details' table
