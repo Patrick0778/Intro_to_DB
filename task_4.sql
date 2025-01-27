57a5c3756c10ee73bc8db628795dd62bcc15429a
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-27 16:01:07 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
 -- Get full description of the table 'books' in the database
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
