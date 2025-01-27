b26a581237eb77a470cd3a7d011e723c9aa0410c
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-27 14:54:38 +0300

    first sql

---
+++ alx_book_store.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
