19085f281dc1bee33442f1e24a8bad167b218a05
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-27 16:03:10 +0300

    my first SQL

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single row into the 'customers' table
+INSERT INTO Customers (customer_id, name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
